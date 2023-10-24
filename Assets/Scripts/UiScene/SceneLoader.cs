a897b3b6952648ec53fa24a59012086b6b804212
TheBlaze <santi.volcanos@gmail.com>
2023-10-24 08:57:03 -0600

    Main Menu and UI

---
+++ Assets/Scripts/UiScene/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void SceneChange(int _scene)
+    { SceneManager.LoadScene(_scene); }
+}
